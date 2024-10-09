bf5ef90f6f92a127230aec426b5393abd62d74f5
Tri Salendah <trisalendah026@student.unsrat.ac.id>
2024-10-09 18:32:56 +0800

    Test

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,35 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import './Main.css';
+import WeatherService from '../services/WeatherService';
 
 const Tab1: React.FC = () => {
+  const weatherData = new WeatherService();
+  const [weather, setWeather]: [any, any] = useState({
+    lokasi: '',
+    suhu: '',
+    keterangan: '',
+    icon: ''
+  })
+
+  useEffect(() => {
+    weatherData.getData().then(data => {
+      setWeather({
+        lokasi: data.name,
+        suhu: data.main.temp,
+        ket: `${data.weather[0].main} (${data.weather[0].description})`,
+        icon: data.weather[0].icon
+      });
+      console.log(data)
+    })
+  }, []);
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Aplikasi Cuaca</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +38,12 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="container">
+          <h1>{weather.suhu}</h1>
+          <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={weather.suhu}/>
+          <h2>{weather.lokasi}</h2>
+          <p>{weather.keterangan}</p>
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,25 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './Main.css';
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Info</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="container">
+          Developed by Tri Salendah
+        </div>
       </IonContent>
     </IonPage>
   );
